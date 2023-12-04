89f18bc93052fbec85fd6f7f8ed302ae0505e2e2
saria <srooalfakiahmed@gmail.com>
2023-12-04 15:18:01 -0800

    a

--- myprintf.c
+++ myprintf.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...)
 {
@@ -11,19 +12,27 @@ int _printf(const char *format, ...)
 
 	while (*p != '\0') {
 		if (*p == '%') {
-			p++; 
-			if (*p == 'c') {
-				int ch = va_arg(args, int);
-				count += write(1, &ch, sizeof(ch));
-			} else if (*p == 's') {
+			p++;  // انتقل إلى المحول التالي
+			if (*p == 's') {
 				char *str = va_arg(args, char *);
 				int len = 0;
 				while (str[len] != '\0')
 					len++;
-				count += write(1, str, len);
-			} else if (*p == '%') {
-				char percent = '%';
-				count += write(1, &percent, sizeof(percent));
+
+				char *temp = (char *)malloc((len + 1) * sizeof(char));
+				if (temp == NULL) {
+					// إدارة حالة فشل الحجز
+					return -1;
+				}
+
+				for (int i = 0; i < len; i++) {
+					temp[i] = str[i];
+				}
+				temp[len] = '\0';
+
+				count += write(1, temp, len);
+
+				free(temp);
 			}
 		} else {
 			count += write(1, p, 1);
