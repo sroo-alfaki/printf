b3ae3901dcddc96dc2166b4d7194f09d0f22ea98
saria <srooalfakiahmed@gmail.com>
2023-12-08 17:03:05 -0800

    my

--- pr_string.c
+++ pr_string.c
@@ -14,22 +14,11 @@ int f_string(va_list args)
 	int count = 0;
 
 	if (s == NULL)
-		s = "null";
+		s = ("null");
 
 	while (*s != '\0')
 	{
-		if (*s < 32 || *s >= 127)
-		{
-			count += f_write('\\');
-			count += f_write('x');
-			count += f_write((*s >> 4) + ((*s >> 4) < 10 ? '0' : 'A' - 10));
-			count += f_write((*s & 0xF) + ((*s & 0xF) < 10 ? '0' : 'A' - 10));
-		}
-		else
-		{
-			count += f_write(*s);
-		}
-
+		count += f_write(*s);
 		s++;
 	}
 
